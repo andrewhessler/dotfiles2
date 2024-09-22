d2b74b0b729639259ed835f9f58daa5fc7824415
Andrew Hessler <adhessler@gmail.com>
2024-09-22 10:06:40 -0500

    sync

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -36,3 +36,7 @@ vim.o.colorcolumn = ""
 vim.o.cursorline = true
 
 vim.g.have_nerd_font = true
+
+vim.diagnostic.config {
+    float = { border = "rounded" },
+}
--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -11,7 +11,7 @@ return {
 
             vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
             vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#334444", fg = "none" })
-            vim.api.nvim_set_hl(0, "FloatBorder", { fg = "Red" })
+            vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#334444", fg = "#EE7722" })
             vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
             vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
             vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })

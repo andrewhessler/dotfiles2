b70e73049805138a46143dfdcf292fb752daf558
Andrew Hessler <adhessler@gmail.com>
2024-09-22 11:19:15 -0500

    sync

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -10,8 +10,8 @@ return {
             vim.cmd.colorscheme(color)
 
             vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#334444", fg = "none" })
-            vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#334444", fg = "#EE7722" })
+            -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#334444", fg = "none" })
+            -- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#334444", fg = "#EE7722" })
             vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
             vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
             vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "none" })

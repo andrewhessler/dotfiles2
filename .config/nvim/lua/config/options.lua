4fe3cddd41fb4e673407af267c58edb7e4cd9acc
Andrew Hessler <andrew.hessler@mastery.net>
2024-10-26 19:56:08 -0500

    sync

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -7,9 +7,9 @@ vim.o.mouse = 'a'
 vim.o.autowrite = true
 -- vim.o.clipboard = "unnamedplus"
 
-vim.o.tabstop = 4
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
 vim.o.expandtab = true
 
 vim.o.smartindent = true

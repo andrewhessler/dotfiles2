7787c2c5527014da24af7e55d3b197e74386759d
Andrew Hessler <adhessler@gmail.com>
2024-11-14 07:54:23 -0600

    sync

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,11 +1,5 @@
 return {
   {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v4.x',
-    lazy = true,
-    config = false,
-  },
-  {
     'williamboman/mason.nvim',
     lazy = false,
     config = true,
@@ -14,7 +8,9 @@ return {
     "pmizio/typescript-tools.nvim",
     dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
     config = function()
-      require("typescript-tools").setup {}
+      require("typescript-tools").setup({
+
+      })
 
       vim.keymap.set('n', '<leader>tir', '<cmd>TSToolsRemoveUnusedImports<cr>')
       vim.keymap.set('n', '<leader>tia', '<cmd>TSToolsAddMissingImports<cr>')
@@ -96,33 +92,34 @@ return {
       { 'j-hui/fidget.nvim',                opts = {} },
     },
     config = function()
-      local lsp_zero = require('lsp-zero')
+      vim.o.signcolumn = 'yes'
 
-      -- lsp_attach is where you enable features that only work
-      -- if there is a language server active in the file
-      local lsp_attach = function(client, bufnr)
-        local opts = { buffer = bufnr }
+      local lspconfig_defaults = require('lspconfig').util.default_config
+      lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+        'force',
+        lspconfig_defaults.capabilities,
+        require('cmp_nvim_lsp').default_capabilities()
+      )
 
-        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-        vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-      end
-
-      lsp_zero.extend_lspconfig({
-        sign_text = true,
-        lsp_attach = lsp_attach,
-        capabilities = require('cmp_nvim_lsp').default_capabilities()
+      vim.api.nvim_create_autocmd('LspAttach', {
+        desc = 'LSP Actions',
+        callback = function(event)
+          local opts = { buffer = event.buf }
+          vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+          vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+          vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+          vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+          vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+          vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+          vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+          vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+          vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+          vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+        end
       })
 
       require('mason-lspconfig').setup({
-        ensure_installed = { 'eslint', 'ts_ls', 'lua_ls', 'zls', 'omnisharp', 'gopls' }, -- 'ts_ls',
+        ensure_installed = { 'eslint', 'denols', 'ts_ls', 'lua_ls', 'zls', 'omnisharp', 'gopls' }, -- 'ts_ls',
         automatic_installation = true,
         handlers = {
           -- this first function is the "default handler"
@@ -131,8 +128,20 @@ return {
             require('lspconfig')[server_name].setup({})
           end,
           ['ts_ls'] = function()
-
-          end
+            --  local lsp = require('lspconfig')
+            --  lsp.ts_ls.setup({
+            --    on_attach = on_attach,
+            --    root_dir = lsp.util.root_pattern("package.json"),
+            --    single_file_support = false
+            --  })
+          end,
+          ['denols'] = function()
+            local lsp = require('lspconfig')
+            lsp.denols.setup({
+              on_attach = on_attach,
+              root_dir = lsp.util.root_pattern("deno.json", "deno.jsonc")
+            })
+          end,
         }
       })
     end

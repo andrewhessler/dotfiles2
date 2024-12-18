f22eb471fa7e818cf18ae64b6862275bede130ad
Andrew Hessler <andrew.hessler@mastery.net>
2024-12-18 16:37:52 -0600

    sync

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -11,7 +11,8 @@ return {
       vim.cmd.colorscheme(color)
 
       vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-      vim.api.nvim_set_hl(0, "CursorLine", { bg = "#334444" })
+      vim.api.nvim_set_hl(0, "CursorLine", { bg = "#23344d" })
+      vim.api.nvim_set_hl(0, "Comment", { bg = "#23344d", fg = "#aaaaaa" }) -- controls git blame highlighting
       -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#334444", fg = "none" })
       -- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#334444", fg = "#EE7722" })
       vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })

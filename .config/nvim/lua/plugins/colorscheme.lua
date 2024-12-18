6140164a7d51b8b75d8e23244caa872ffbf5955a
Andrew Hessler <andrew.hessler@mastery.net>
2024-12-18 11:04:34 -0600

    sync

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -11,6 +11,7 @@ return {
       vim.cmd.colorscheme(color)
 
       vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "CursorLine", { bg = "#334444" })
       -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#334444", fg = "none" })
       -- vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#334444", fg = "#EE7722" })
       vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })

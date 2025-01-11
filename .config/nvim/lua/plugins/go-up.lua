e183a66624816390a498208658cfd62ef53d9ed0
Andrew Hessler <adhessler@gmail.com>
2025-01-11 17:40:23 -0600

    sync

--- .config/nvim/lua/plugins/go-up.lua
+++ .config/nvim/lua/plugins/go-up.lua
@@ -1,6 +1,8 @@
 return {
   'nullromo/go-up.nvim',
-  opts = {},   -- specify options here
+  opts = {
+    respectScrolloff = true
+  }, -- specify options here
   config = function(_, opts)
     local goUp = require('go-up')
     goUp.setup(opts)

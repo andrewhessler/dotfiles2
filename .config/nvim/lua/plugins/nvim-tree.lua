ebac4e3fa5bce5d9939ec97b36e6c5d11afb307d
Andrew Hessler <andrew.hessler@mastery.net>
2025-02-10 16:43:38 -0600

    sync

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -49,6 +49,17 @@ return {
     'nvim-tree/nvim-web-devicons',
   },
   {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
+  {
     "nvim-tree/nvim-tree.lua",
     config = function()
       local nvimtree = require("nvim-tree")

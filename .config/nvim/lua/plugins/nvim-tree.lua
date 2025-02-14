b3754f612742cb934edc9489baff19e6bb010770
Andrew Hessler <andrew.hessler@mastery.net>
2025-02-13 21:18:01 -0600

    sync

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -58,6 +58,10 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
+    config = function()
+      require("oil").setup()
+      vim.keymap.set("n", "-", "<cmd>Oil<CR>", { noremap = true, silent = true })
+    end
   },
   {
     "nvim-tree/nvim-tree.lua",
